6c4680b6b4e69cb3bf1e22ffbd76ca5834115932
QuadroPhazon <quadrophazon.4@gmail.com>
2016-04-17 16:48:57 +0200

    Username
    Ajout username et changement de librairie openlauncherlib 3.0 ->
    openlauncherlib 2.1

--- src/fr/metarum/Excalia/launcher/Launcher.java
+++ src/fr/metarum/Excalia/launcher/Launcher.java
@@ -2,15 +2,15 @@ package fr.metarum.Excalia.launcher;
 
 import java.io.File;
 
-import fr.theshark34.openlauncherlib.minecraft.GameInfos;
-import fr.theshark34.openlauncherlib.minecraft.GameTweak;
-import fr.theshark34.openlauncherlib.minecraft.GameType;
-import fr.theshark34.openlauncherlib.minecraft.GameVersion;
+import fr.theshark34.openlauncherlib.launcher.GameInfos;
+import fr.theshark34.openlauncherlib.launcher.GameTweak;
+import fr.theshark34.openlauncherlib.launcher.GameType;
+import fr.theshark34.openlauncherlib.launcher.GameVersion;
 
 public class Launcher {
 
 	public static final GameVersion E_VERSION = new GameVersion("1.8.4", GameType.V1_8_HIGHER);
-	public static final GameInfos E_INFOS = new GameInfos("Excalia", E_VERSION, new GameTweak[]{GameTweak.FORGE});
+	public static final GameInfos E_INFOS = new GameInfos("Excalia", E_VERSION, true, new GameTweak[]{GameTweak.FORGE});
 	public static final File E_DIR = E_INFOS.getGameDir();
 
 }
--- src/fr/metarum/Excalia/launcher/LauncherPanel.java
+++ src/fr/metarum/Excalia/launcher/LauncherPanel.java
@@ -4,18 +4,29 @@ import java.awt.Graphics;
 import java.awt.Image;
 
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
+import fr.theshark34.openlauncherlib.launcher.util.UsernameSaver;
 import fr.theshark34.swinger.Swinger;
 
 @SuppressWarnings("serial")
 public class LauncherPanel extends JPanel {
 
-	private Image background = Swinger.getResource("");
+	private Image background = Swinger.getResource("background.png");
 	
+	private UsernameSaver saver = new UsernameSaver(Launcher.E_INFOS);
+	private JTextField usernameField = new JTextField(saver.getUsername(""));
+	
+	public LauncherPanel(){
+		
+		
+	}
 	
 	@Override
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
+		
+		g.drawImage(background, 0, 0, this.getWidth(), this.getHeight(), this);
 	}
 	
 }
