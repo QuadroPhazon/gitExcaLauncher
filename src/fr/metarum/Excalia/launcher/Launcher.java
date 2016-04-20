1edc62cb54616722e7b5d0f3935fdb76ad8d6f4b
QuadroPhazon <quadrophazon.4@gmail.com>
2016-04-20 22:47:12 +0200

    Début Update
    Création authentification + exceptions //A VOIR : CRACK
    création thread
    création progressbar
    début création update // Début Vidéo 2

--- src/fr/metarum/Excalia/launcher/Launcher.java
+++ src/fr/metarum/Excalia/launcher/Launcher.java
@@ -2,15 +2,37 @@ package fr.metarum.Excalia.launcher;
 
 import java.io.File;
 
+import fr.theshark34.openauth.AuthPoints;
+import fr.theshark34.openauth.AuthenticationException;
+import fr.theshark34.openauth.Authenticator;
+import fr.theshark34.openauth.model.AuthAgent;
+import fr.theshark34.openauth.model.response.AuthResponse;
+import fr.theshark34.openlauncherlib.launcher.AuthInfos;
 import fr.theshark34.openlauncherlib.launcher.GameInfos;
 import fr.theshark34.openlauncherlib.launcher.GameTweak;
 import fr.theshark34.openlauncherlib.launcher.GameType;
 import fr.theshark34.openlauncherlib.launcher.GameVersion;
+import fr.theshark34.supdate.SUpdate;
 
 public class Launcher {
 
 	public static final GameVersion E_VERSION = new GameVersion("1.8.4", GameType.V1_8_HIGHER);
 	public static final GameInfos E_INFOS = new GameInfos("Excalia", E_VERSION, true, new GameTweak[]{GameTweak.FORGE});
 	public static final File E_DIR = E_INFOS.getGameDir();
-
+	
+	private static AuthInfos authInfos;
+	
+	public static void auth(String username, String password) throws AuthenticationException
+	{
+		Authenticator authenticator = new Authenticator(Authenticator.MOJANG_AUTH_URL, AuthPoints.NORMAL_AUTH_POINTS);
+		AuthResponse response = authenticator.authenticate(AuthAgent.MINECRAFT, username, password, "");
+		authInfos = new AuthInfos(response.getSelectedProfile().getName(), response.getAccessToken(), response.getSelectedProfile().getId());
+		
+	}
+	
+	public static void update() throws Exception
+	{
+		SUpdate su = new SUpdate("");
+	}
+	
 }
--- src/fr/metarum/Excalia/launcher/LauncherPanel.java
+++ src/fr/metarum/Excalia/launcher/LauncherPanel.java
@@ -7,12 +7,17 @@ import java.awt.Graphics;
 import java.awt.Image;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
+import fr.theshark34.openauth.AuthenticationException;
 import fr.theshark34.openlauncherlib.launcher.util.UsernameSaver;
 import fr.theshark34.swinger.Swinger;
+import fr.theshark34.swinger.colored.SColoredBar;
 import fr.theshark34.swinger.event.SwingerEvent;
 import fr.theshark34.swinger.event.SwingerEventListener;
 import fr.theshark34.swinger.textured.STexturedButton;
@@ -35,6 +40,10 @@ public class LauncherPanel extends JPanel implements SwingerEventListener{
 	
 	private STexturedButton quitButton = new STexturedButton(Swinger.getResource("quit.png"));
 	private STexturedButton reductButton = new STexturedButton(Swinger.getResource("reduct.png"));
+	
+	
+	private SColoredBar progressBar = new SColoredBar(Swinger.getTransparentWhite(100), new Color(0,255,0,175));
+	private JLabel infoLabel = new JLabel("Clique sur OK !", SwingConstants.CENTER);
 	
 	public LauncherPanel(){
 		this.setLayout(null);
@@ -83,6 +92,14 @@ public class LauncherPanel extends JPanel implements SwingerEventListener{
 		this.add(reductButton);
 		
 		
+		progressBar.setBounds(33, 603, 850, 13);
+		this.add(progressBar);
+		
+		infoLabel.setForeground(Color.BLUE);
+		infoLabel.setFont(usernameField.getFont());
+		infoLabel.setBounds(33, 573, 850, 18);
+		this.add(infoLabel);
+		
 	} //END LAUNCHERPANEL
 	
 	@Override
@@ -90,7 +107,29 @@ public class LauncherPanel extends JPanel implements SwingerEventListener{
 		
 		if(e.getSource() == OKButton)
 		{
-			System.out.println("click!");
+			setFieldsEnabled(false);
+			
+			if(usernameField.getText().replaceAll(" ", "").length()==0 || passwordField.getText().length() == 0)
+			{
+				JOptionPane.showMessageDialog(this, "Erreur, merci d'entrer un pseudo et/ou un mot de passe valides", "Erreur", JOptionPane.ERROR_MESSAGE);
+				setFieldsEnabled(true);
+				return;
+			}
+			
+			Thread t = new Thread(){
+				@Override
+				public void run(){
+					try {
+						Launcher.auth(usernameField.getText(), passwordField.getText());
+					} catch (AuthenticationException e) {
+						JOptionPane.showMessageDialog(LauncherPanel.this, "Erreur, impossible de se connecter : " + e.getErrorModel().getErrorMessage(), "Erreur", JOptionPane.ERROR_MESSAGE);
+						setFieldsEnabled(true);
+						return;
+					}
+					
+				}
+			};
+			t.start();
 		}
 		else if(e.getSource() == quitButton)
 		{
@@ -113,5 +152,21 @@ public class LauncherPanel extends JPanel implements SwingerEventListener{
 		
 		g.drawImage(background, 0, 0, this.getWidth(), this.getHeight(), this);
 	} //END PAINTCOMPONENT
+	
+	public void setFieldsEnabled(boolean enabled)
+	{
+		usernameField.setEnabled(enabled);
+		passwordField.setEnabled(enabled);
+		OKButton.setEnabled(enabled);
+	}
+	
+	
+	public SColoredBar getProgressBar(){
+		return progressBar;
+	}
+	
+	public void setInfoText(String text){
+		infoLabel.setText(text);
+	}
 	
 }//END LAUNCHERPANEL
